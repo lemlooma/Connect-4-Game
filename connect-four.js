37e1d34d65a0d1f4d380d31a033f133bf074a0fb
Levi-Dominguez <josue.dominguez2@g.austincc.edu>
2021-05-05 13:00:07 -0700

    indicated the current player

--- connect-four.js
+++ connect-four.js
@@ -1,6 +1,7 @@
 import { Game } from './game.js';
 
-let game = undefined; 
+let game = undefined;
+let clickTarget = document.getElementById("click-targets");
 
 function updatedUi() {
     const boardHolder = document.getElementById("board-holder");
@@ -12,34 +13,51 @@ function updatedUi() {
     }else {
         boardHolder.classList.remove("is-invisble");
         gameName.innerHTML = game.getName()
+        const currentPlayer = game.currentPlayer;
+
+        if (currentPlayer === 1) {
+            clickTarget.classList.add('black');
+            clickTarget.classList.remove('red');
+        } else {
+            clickTarget.classList.add('red');
+            clickTarget.classList.remove('black');
+        }
+
     }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-const name1 = document.getElementById("player-1-name");
-const name2 = document.getElementById("player-2-name");
-const newGameButt = document.getElementById("new-game");
+    const name1 = document.getElementById("player-1-name");
+    const name2 = document.getElementById("player-2-name");
+    const newGameButt = document.getElementById("new-game");
 
-function enabledButt () {
-    let name1Val =name1.value;
-    let name2Val = name2.value;
 
-    newGameButt.disabled = name1Val.length === 0 || name2Val.length === 0
-}
+    function enabledButt () {
+        let name1Val = name1.value;
+        let name2Val = name2.value;
+
+        newGameButt.disabled = name1Val.length === 0 || name2Val.length === 0
+    }
+
     name1.addEventListener("keyup", (event) => {
         enabledButt()
     })
 
-     name2.addEventListener("keyup", (event) => {
+    name2.addEventListener("keyup", (event) => {
         enabledButt()
     })
-    
+
     newGameButt.addEventListener('click', (event)=> {
         game = new Game(name1.value, name2.value);
         name1.value = "";
         name2.value = "";
         enabledButt()
         updatedUi()
+    })
+
+    clickTarget.addEventListener("click", event => {
+        game.playInColumn();
+        updatedUi();
     })
 
 })
--- game.js
+++ game.js
@@ -2,8 +2,16 @@ export class Game {
     constructor(playerOneName,playerTwoName,) {
         this.playerOneName = playerOneName;
         this.playerTwoName = playerTwoName;
+        this.currentPlayer = 1;
     }
     getName() {
         return `${this.playerOneName} VS ${this.playerTwoName}`;
+    }
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            this.currentPlayer = 2;
+        } else {
+            this.currentPlayer = 1;
+        }
     }
 }
